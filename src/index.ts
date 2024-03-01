41b73bb4cc7719898d024c0f7989352de7d06437
H4M5TER <h4m5ter@outlook.com>
2024-03-02 00:48:20 +0800

    fix: adapt cordis 3.12.0

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,7 @@ export function apply(context: Context, config: Config) {
       initial: {},
     },
   })
-  const ctx = context.isolate(['http'])
+  const ctx = context.isolate('http')
   ctx.http = context.http.extend({
     headers: {
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
